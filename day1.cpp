6d84bcf5303fda41e2aca89d95ffd3dfb1f94c8c
Muzammil Hussain <muzammil.org73@gmail.com>
2025-10-23 21:25:00 +0500

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a ;
+    int b;
+    cout <<"enter the first number "<<endl;
+    cin >>a;
+    cout <<"enter the scond number "<<endl;
+    cin >>b;
+    return 0;
+}
