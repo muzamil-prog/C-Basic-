fa197bb2b0136549f067b01d654b628d6aa30b7a
Muzammil Hussain <muzammil.org73@gmail.com>
2025-10-23 21:25:59 +0500

    Add files via upload

---
+++ adds two number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a ;
+    int b;
+    cout <<"enter the first number "<<endl;
+    cin >>a;
+    cout <<"enter the scond number "<<endl;
+    cin >>b;
+    int result = a+b;
+    cout <<"result is "<< result<<endl;
+    
+    return 0;
+}
