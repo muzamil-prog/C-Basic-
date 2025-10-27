ed51c48c432237e3ca4787bf9dd43f638c44f393
Muzammil Hussain <muzammil.org73@gmail.com>
2025-10-27 21:41:36 +0500

    Add files via upload

---
+++ asccivalue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string ma = "HELLO";
+    cout <<" ascii value of hello is are:";
+    for(char c : ma){
+        cout << int(c)<< " ";
+
+    } 
+    cout <<endl;
+}
+
