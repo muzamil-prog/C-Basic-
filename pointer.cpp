72fb5694ef17c4d5c1861554c18dd575bed13aa0
Muzammil Hussain <muzammil.org73@gmail.com>
2025-11-09 23:07:47 +0500

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int num[6];
+ for(int i= 0; i<6; i++){
+
+ cin>> num[i]; }
+
+ cout<< endl;
+  for(int i= 0; i<6; i++){
+  cout << num[i];}
+
+  cout <<endl;
+
+
+ return 0;}
